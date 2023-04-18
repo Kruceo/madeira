71acc097c87a85bac461d554f5fb2c2dc615c595
kruceo <rafa2015krueger@gmail.com>
2023-04-18 10:01:40 -0300

    fix: time format

--- index.mjs
+++ index.mjs
@@ -13,28 +13,28 @@ class Logger {
     constructor(local,options) {
         this.time = () => {
             const date = (new Date())
-            const time = date.toISOString().replace('T',' ').replace('Z','')
-            return time
+            const time = date.toISOString().replace('Z','')
+            return time.split('T')
         }
         this.local = local
         this.format = options?options.format??'txt':'txt'
 
         if(!fs.existsSync(this.local))fs.mkdirSync(this.local,{recursive:true})
         this.error = (text) => {
-            console.log(coloral.markred('[ERR]') + ' ' + this.time() + ' # ' + text)
-            if(this.local)fs.appendFileSync(path.resolve(this.local,this.time().split(' ')[0] + '.' + this.format),'[ERR] '+this.time() + ' # ' + text+'\n')
+            console.log(coloral.markred('[ERR]') + ' ' + this.time()[1] + ' # ' + text)
+            if(this.local)fs.appendFileSync(path.resolve(this.local,this.time()[0] + '.' + this.format),'[ERR] '+this.time() + ' # ' + text+'\n')
         }
         this.info = (text) => {
-            console.log(coloral.markblue('[INF]') + ' ' + this.time() + ' # ' + text)
-            if(this.local)fs.appendFileSync(path.resolve(this.local,this.time().split(' ')[0] + '.' + this.format),'[INF] '+this.time() + ' # ' + text+'\n')
+            console.log(coloral.markblue('[INF]') + ' ' + this.time()[1] + ' # ' + text)
+            if(this.local)fs.appendFileSync(path.resolve(this.local,this.time()[0] + '.' + this.format),'[INF] '+this.time() + ' # ' + text+'\n')
         }
         this.warn = (text) => {
-            console.log(coloral.markyellow('[WRN]') + ' ' + this.time() + ' # ' + text)
-            if(this.local)fs.appendFileSync(path.resolve(this.local,this.time().split(' ')[0] + '.' + this.format),'[WRN] '+this.time() + ' # ' + text+'\n')
+            console.log(coloral.markyellow('[WRN]') + ' ' + this.time()[1] + ' # ' + text)
+            if(this.local)fs.appendFileSync(path.resolve(this.local,this.time()[0] + '.' + this.format),'[WRN] '+this.time() + ' # ' + text+'\n')
         }
         this.done = (text) => {
-            console.log(coloral.markgreen('[DON]') + ' ' + this.time() + ' # ' + text)
-            if(this.local)fs.appendFileSync(path.resolve(this.local,this.time().split(' ')[0] + '.' + this.format),'[DON] '+this.time() + ' # ' + text+'\n')
+            console.log(coloral.markgreen('[DON]') + ' ' + this.time()[1] + ' # ' + text)
+            if(this.local)fs.appendFileSync(path.resolve(this.local,this.time()[0] + '.' + this.format),'[DON] '+this.time() + ' # ' + text+'\n')
         }
     }
 }
--- readme.md
+++ readme.md
@@ -14,10 +14,10 @@ logger.warn('this is a warn!')
 
 ```js
 // >> ./logs/2023-04-18.txt
-[ERR] 2023-04-18 12:31:06.642 # this is a error! //the first symbol is red
-[INF] 2023-04-18 12:31:06.643 # this is a info!  //the first symbol is blue
-[DON] 2023-04-18 12:31:06.644 # this is done!    //the first symbol is green
-[WRN] 2023-04-18 12:31:06.645 # this is a warn!  //the first symbol is yellow
+[ERR] 12:31:06.642 # this is a error! //the first symbol is red
+[INF] 12:31:06.643 # this is a info!  //the first symbol is blue
+[DON] 12:31:06.644 # this is done!    //the first symbol is green
+[WRN] 12:31:06.645 # this is a warn!  //the first symbol is yellow
 ```
 **To install:**
 
